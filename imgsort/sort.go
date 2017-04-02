a2ac6eaf43f795c5484388c498e365b3e34232da
Alex Nichol <unixpickle@gmail.com>
2017-04-02 18:03:40 -0400

    implemented sort tool

--- imgsort/sort.go
+++ imgsort/sort.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+
+	"github.com/unixpickle/essentials"
+)
+
 func Sort(args []string) {
+	var sorterPath string
+
+	fs := flag.NewFlagSet("sort", flag.ExitOnError)
+	fs.StringVar(&sorterPath, "data", "sort_data", "where to read sort results")
+	fs.Parse(args)
+
+	sorter, err := ReadSorter(sorterPath)
+	if err != nil {
+		essentials.Die(err)
+	}
+
+	sorted := sorter.Sort()
+	for _, x := range sorted {
+		fmt.Println(x)
+	}
 }
