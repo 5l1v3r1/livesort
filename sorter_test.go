2cdf98f386e99a4f58111a772ffd91dffbcf47e0
Alex Nichol <unixpickle@gmail.com>
2017-04-03 16:02:28 -0400

    benchmark sort in CPU time

--- sorter_test.go
+++ sorter_test.go
@@ -51,3 +51,28 @@ func TestReject(t *testing.T) {
 		t.Fatal("wrongful rejection")
 	}
 }
+
+func BenchmarkSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		initial := make([]interface{}, 100)
+		for i, j := range rand.Perm(len(initial)) {
+			initial[i] = j
+		}
+		sorter := &Sorter{Elements: initial}
+		for {
+			n1, n2 := sorter.Request()
+			if n1 == nil {
+				break
+			}
+			var res bool
+			if n1.(int) < n2.(int) {
+				res = sorter.Add(n1, n2)
+			} else {
+				res = sorter.Add(n2, n1)
+			}
+			if !res {
+				b.Fatal("comparison rejected")
+			}
+		}
+	}
+}
