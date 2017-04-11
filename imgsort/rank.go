c3839d569b99d450ce87f8554ae4cc3de5072a9a
Alex Nichol <unixpickle@gmail.com>
2017-04-11 12:11:37 -0400

    ranking edge dropout
    This is kind of like dropout or bootstrap aggregating. We delete a random subset of the edges at each iteration to prevent "overfitting".

--- imgsort/rank.go
+++ imgsort/rank.go
@@ -3,18 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"sort"
 
 	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/livesort"
 )
 
 func Rank(args []string) {
 	var sorterPath string
 	var iters int
+	var edgeFrac float64
 
 	fs := flag.NewFlagSet("rank", flag.ExitOnError)
 	fs.StringVar(&sorterPath, "data", "sort_data", "where to read sort results")
 	fs.IntVar(&iters, "iters", 100, "number of iterations")
+	fs.Float64Var(&edgeFrac, "edgefrac", 1, "fraction of edges to keep per iteration")
 	fs.Parse(args)
 
 	sorter, err := ReadSorter(sorterPath)
@@ -25,7 +29,7 @@ func Rank(args []string) {
 	rankings := map[interface{}]float64{}
 
 	for i := 0; i < iters; i++ {
-		for j, elem := range sorter.Sort() {
+		for j, elem := range dropEdges(sorter, edgeFrac).Sort() {
 			rankings[elem] += float64(j) / float64(iters*(len(sorter.Elements)-1))
 		}
 	}
@@ -41,6 +45,22 @@ func Rank(args []string) {
 	for _, item := range list {
 		fmt.Println(item.Rank, item.Elem)
 	}
+}
+
+func dropEdges(sorter *livesort.Sorter, keepFrac float64) *livesort.Sorter {
+	if keepFrac == 1 {
+		return sorter
+	}
+	perm := rand.Perm(len(sorter.Comparisons))
+	keepNum := int(keepFrac*float64(len(sorter.Comparisons)) + 0.5)
+	if keepNum < 0 || keepNum > len(sorter.Comparisons) {
+		essentials.Die("bad edge fraction")
+	}
+	res := &livesort.Sorter{Elements: sorter.Elements}
+	for _, i := range perm[:keepNum] {
+		res.Comparisons = append(res.Comparisons, sorter.Comparisons[i])
+	}
+	return res
 }
 
 type rankedElem struct {
